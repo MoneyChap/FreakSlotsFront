3e5657660585f4f6339ce9fdeb47138284624b91
MoneyChap <eriks.osis@whykingz.com>
2026-01-07 23:36:59 +0200

    Design work

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,96 +1,58 @@
 // src/components/SplashScreen.jsx
-export default function SplashScreen({ phase = "spinning", winEmoji = "💎" }) {
-    const isWin = phase === "win";
-
+export default function SplashScreen() {
     return (
         <div className="splashRoot" role="status" aria-label="Loading">
-            <div className={`splashCard ${isWin ? "splashCardWin" : ""}`}>
+            <div className="splashCard">
                 <div className="splashBrand">
                     <div className="splashLogo">
-                        <div className={`splashReels ${isWin ? "splashReelsWin" : ""}`}>
+                        <div className="splashReels">
                             <div className="reel">
-                                <div className={`reelTrack ${isWin ? "reelTrackFreeze" : ""}`}>
-                                    {isWin ? (
-                                        <>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <span>🍒</span>
-                                            <span>🔔</span>
-                                            <span>💎</span>
-                                            <span>7</span>
-                                            <span>🍋</span>
-                                            <span>🎁</span>
-                                            <span>🍒</span>
-                                            <span>🔔</span>
-                                        </>
-                                    )}
+                                <div className="reelTrack reelSpin1">
+                                    <span>🍒</span>
+                                    <span>🔔</span>
+                                    <span>💎</span>
+                                    <span>7</span>
+                                    <span>🍋</span>
+                                    <span>🎁</span>
+                                    <span>🍒</span>
+                                    <span>🔔</span>
                                 </div>
                             </div>
 
                             <div className="reel">
-                                <div
-                                    className={`reelTrack reelTrack2 ${isWin ? "reelTrackFreeze" : ""}`}
-                                >
-                                    {isWin ? (
-                                        <>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <span>💎</span>
-                                            <span>7</span>
-                                            <span>🍋</span>
-                                            <span>🔔</span>
-                                            <span>🎁</span>
-                                            <span>🍒</span>
-                                            <span>💎</span>
-                                            <span>7</span>
-                                        </>
-                                    )}
+                                <div className="reelTrack reelSpin2">
+                                    <span>💎</span>
+                                    <span>7</span>
+                                    <span>🍋</span>
+                                    <span>🔔</span>
+                                    <span>🎁</span>
+                                    <span>🍒</span>
+                                    <span>💎</span>
+                                    <span>7</span>
                                 </div>
                             </div>
 
                             <div className="reel">
-                                <div
-                                    className={`reelTrack reelTrack3 ${isWin ? "reelTrackFreeze" : ""}`}
-                                >
-                                    {isWin ? (
-                                        <>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                            <span className="reelWinSymbol">{winEmoji}</span>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <span>🍋</span>
-                                            <span>🍒</span>
-                                            <span>7</span>
-                                            <span>💎</span>
-                                            <span>🎁</span>
-                                            <span>🔔</span>
-                                            <span>🍋</span>
-                                            <span>🍒</span>
-                                        </>
-                                    )}
+                                <div className="reelTrack reelSpin3">
+                                    <span>🍋</span>
+                                    <span>🍒</span>
+                                    <span>7</span>
+                                    <span>💎</span>
+                                    <span>🎁</span>
+                                    <span>🔔</span>
+                                    <span>🍋</span>
+                                    <span>🍒</span>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     <div className="splashTitle">FreakSlots</div>
-                    <div className="splashSubtitle">
-                        {isWin ? "Ready" : "Loading games"}
-                    </div>
+                    <div className="splashSubtitle">Loading games</div>
                 </div>
 
                 <div className="splashProgress">
-                    <div className={`splashBar ${isWin ? "splashBarWin" : ""}`} />
+                    <div className="splashBar" />
                 </div>
             </div>
         </div>
